bb38fece9a224209244b8e84cee497f446082d31
Shubham-Somani <sshubhamsomani@gmail.com>
2021-05-09 20:26:46 +0530

    new version publish

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 // ...
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { S3Client, PutObjectCommand, ServiceInputTypes, GetObjectCommand } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  PutObjectCommand,
+  ServiceInputTypes,
+  GetObjectCommand,
+  ListBucketsCommand,
+  ServiceOutputTypes,
+  S3ClientResolvedConfig,
+  ListBucketsCommandOutput,
+  CreateBucketCommand,
+  CreateBucketCommandOutput,
+  CreateBucketCommandInput,
+  DeleteBucketCommandInput,
+  DeleteBucketCommandOutput,
+  DeleteBucketCommand,
+  ListObjectsCommand,
+  ListObjectsCommandInput,
+  ListObjectsCommandOutput
+} from '@aws-sdk/client-s3';
 import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
 import { fromCognitoIdentityPool } from '@aws-sdk/credential-provider-cognito-identity';
-import { Client } from '@aws-sdk/smithy-client';
-import { MetadataBearer } from '@aws-sdk/types';
+import { Client as __Client } from '@aws-sdk/smithy-client';
+import { HttpHandlerOptions as __HttpHandlerOptions } from '@aws-sdk/types';
 import { InitParams, PresignGetUrlParams, PresignPutUrlParams } from './index.types';
 // ...
 
 export class Envoltorio {
-  S3: Client<any, ServiceInputTypes, MetadataBearer, any>;
+  S3: __Client<__HttpHandlerOptions, ServiceInputTypes, ServiceOutputTypes, S3ClientResolvedConfig>
 
   constructor(parameters: InitParams) {
     // Initialize S3 Client
@@ -46,5 +64,37 @@ export class Envoltorio {
     };
     const command = new GetObjectCommand(putObjectParams);
     return await getSignedUrl(this.S3, command, { expiresIn: putParams.expires || 3600 });
+  }
+  
+  /***
+   * List Buckets
+   ***/
+  async getBucketList(): Promise<ListBucketsCommandOutput> {
+    const command = new ListBucketsCommand({});
+    return await this.S3.send(command);
+  }
+
+  /***
+   * Create Bucket
+   ***/
+  async createBucket(createBucketParams: CreateBucketCommandInput): Promise<CreateBucketCommandOutput> {
+    const command = new CreateBucketCommand(createBucketParams);
+    return await this.S3.send(command);
+  }
+
+  /***
+   * Delete Bucket
+   ***/
+  async deleteBucket(deleteBucketParams: DeleteBucketCommandInput): Promise<DeleteBucketCommandOutput> {
+    const command = new DeleteBucketCommand(deleteBucketParams);
+    return await this.S3.send(command);
+  }
+
+  /***
+   * List Objects
+   ***/
+  async getObjectList(listObjectParams: ListObjectsCommandInput): Promise<ListObjectsCommandOutput> {
+    const command = new ListObjectsCommand(listObjectParams);
+    return await this.S3.send(command);
   }
 }
